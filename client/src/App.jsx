57a30f3a6ecb4e4cbff706de27c63cef59d14464
kunal <kunalsawant4142@gmail.com>
2023-08-23 18:13:37 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { Link, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
+import Layout from "./Layout";
+import RegisterPage from "./pages/RegisterPage";
+import axios from "axios";
+import { UserContextProvider } from "./UserContext";
+
+axios.defaults.baseURL = "http://localhost:4000";
+axios.defaults.withCredentials = true;
+
+function App() {
+  return (
+    <>
+      <UserContextProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<IndexPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+          </Route>
+
+          {/* 
+         
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/account" element={<ProfilePage />} />
+          <Route path="/account/places" element={<PlacesPage />} />
+          <Route path="/account/places/new" element={<PlacesFormPage />} />
+          <Route path="/account/places/:id" element={<PlacesFormPage />} />
+          <Route path="/place/:id" element={<PlacePage />} />
+          <Route path="/account/bookings" element={<BookingsPage />} />
+          <Route path="/account/bookings/:id" element={<BookingPage />} />
+        </Route> */}
+        </Routes>
+      </UserContextProvider>
+    </>
+  );
+}
+
+export default App;
